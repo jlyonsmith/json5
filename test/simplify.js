fe300689a5292d9a0a47ad1142e33e91bb2e46fc
John Lyon-Smith <john@lyon-smith.org>
2019-07-22 15:13:07 -0700

    Fix lint issues

--- test/simplify.js
+++ test/simplify.js
@@ -1,4 +1,3 @@
-const assert = require('assert')
 const JSON5 = require('../lib')
 
 require('tap').mochaGlobals()
@@ -11,7 +10,7 @@ t.test('simplify(node)', t => {
         t.strictSame(JSON5.simplify({}), {}, 'leaves empty objects')
         t.strictSame(JSON5.simplify({value: 10}), {value: 10}, 'leaves object without type property')
         t.strictSame(JSON5.simplify({type: 10}), {type: 10}, 'leaves object without value property')
-        t.strictSame(JSON5.simplify([1,2,3]), [1,2,3], 'leaves arrays')
+        t.strictSame(JSON5.simplify([1, 2, 3]), [1, 2, 3], 'leaves arrays')
         t.end()
     })
 
@@ -24,19 +23,19 @@ t.test('simplify(node)', t => {
     })
 
     t.test('objects', t => {
-        t.strictSame(JSON5.simplify({type: 'object', value: {a: {type: 'numeric', value: 1}}}), {a:1}, 'simplifies single node')
-        t.strictSame(JSON5.simplify({type: 'object', value: {abc: {type: 'numeric', value: 1}, def: {type: 'numeric', value: 2}}}), {abc:1,def:2}, 'simplifies multiple properties')
-        t.strictSame(JSON5.simplify({type: 'object', value: {a: {type: 'object', value: {b: {type: 'numeric', value: 2}}}}}), {a:{b:2}}, 'simplifies nested objects')
+        t.strictSame(JSON5.simplify({type: 'object', value: {a: {type: 'numeric', value: 1}}}), {a: 1}, 'simplifies single node')
+        t.strictSame(JSON5.simplify({type: 'object', value: {abc: {type: 'numeric', value: 1}, def: {type: 'numeric', value: 2}}}), {abc: 1, def: 2}, 'simplifies multiple properties')
+        t.strictSame(JSON5.simplify({type: 'object', value: {a: {type: 'object', value: {b: {type: 'numeric', value: 2}}}}}), {a: {b: 2}}, 'simplifies nested objects')
         t.end()
     })
 
     t.test('arrays', t => {
         t.strictSame(JSON5.simplify({type: 'array', value: []}), [], 'simplifies empty arrays')
         t.strictSame(JSON5.simplify({type: 'array', value: [{type: 'numeric', value: 1}]}), [1], 'simplifies array values')
-        t.strictSame(JSON5.simplify({type: 'array', value: [{type: 'numeric', value: 1}, {type: 'numeric', value: 2}]}), [1,2], 'simplifies multiple array values')
-        t.strictSame(JSON5.simplify({type: 'array', value: [{type: 'numeric', value: 1}, {type: 'array', value: [{type: 'numeric', value: 2}]}]}), [1,[2]], 'simplifies nested arrays')
+        t.strictSame(JSON5.simplify({type: 'array', value: [{type: 'numeric', value: 1}, {type: 'numeric', value: 2}]}), [1, 2], 'simplifies multiple array values')
+        t.strictSame(JSON5.simplify({type: 'array', value: [{type: 'numeric', value: 1}, {type: 'array', value: [{type: 'numeric', value: 2}]}]}), [1, [2]], 'simplifies nested arrays')
         t.end()
-     })
+    })
 
     t.test('errors', t => {
         t.throws(() => {
